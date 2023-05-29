8c446dbeaef6cdd902122416502ce983602cf15c
AtennopVer <anton7185569@gmail.com>
2023-05-29 21:47:54 +0300

    Added project for tests

---
+++ Relational-Databases-via-OOP.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace Relational_Databases_via_OOP.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
