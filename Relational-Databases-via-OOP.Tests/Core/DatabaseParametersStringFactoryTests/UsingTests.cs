4f9a45dc2c532754922c2e697248e41ac059821b
AtennopVer <anton7185569@gmail.com>
2023-06-01 19:46:08 +0300

    Added tests for DatabaseParametersStringFactory

---
+++ Relational-Databases-via-OOP.Tests/Core/DatabaseParametersStringFactoryTests/UsingTests.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+
+namespace RelationalDatabasesViaOOP.Tests.DatabaseParametersStringFactoryTests
+{
+    public sealed class UsingTests
+    {
+        private IDatabaseParametersStringFactory _factory;
+
+        [SetUp]
+        public void Setup()
+            => _factory = new DatabaseParametersStringFactory();
+
+        [Test]
+        public void IsCreatingCorrect1()
+            => Assert.Throws<ArgumentNullException>(() => _factory.Create(null, " AND "));
+        
+        [Test]
+        public void IsCreatingCorrect2()
+            => Assert.Throws<ArgumentNullException>(() => _factory.Create(new string[] { }, null));
+        
+        [Test]
+        public void IsCreatingCorrect3()
+            => Assert.That(_factory.Create(new string[] { }, " AND "), Is.EqualTo(string.Empty));
+        
+        [Test]
+        public void IsCreatingCorrect4()
+            => Assert.That(_factory.Create(new[] { "hello", "goodbye" }, string.Empty), Is.EqualTo("hellogoodbye"));
+        
+        [Test]
+        public void IsCreatingCorrect5()
+            => Assert.That(_factory.Create(new[] { "hello", "goodbye" }, " AND "), Is.EqualTo("hello AND goodbye"));
+    }
+}
