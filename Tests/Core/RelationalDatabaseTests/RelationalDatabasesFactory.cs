4487818bc2ae2d4415f89d0bafc2ee89d7c9507c
AtennopVer <anton7185569@gmail.com>
2023-06-08 14:51:31 +0300

    Removed some files

--- Tests/Core/RelationalDatabaseTests/RelationalDatabasesFactory.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace RelationalDatabasesViaOOP.Tests.Core.RelationalDatabaseTests
-{
-    public sealed class RelationalDatabasesFactory
-    {
-        public IDatabase Create()
-            => new RelationalDatabase(@"Server=localhost;Port=5434;User Id=postgres;Password=igay123Aa;Database=DatabaseForLibraryTests");
-    }
-}
