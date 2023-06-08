d7d0ac80890010b49030cc048f151fb0864bf5fd
AtennopVer <anton7185569@gmail.com>
2023-06-08 15:07:08 +0300

    Added new tests for RelationalDatabase

---
+++ Tests/Core/RelationalDatabaseTests/UsingTests.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using NUnit.Framework;
+
+namespace RelationalDatabasesViaOOP.Tests.Core.RelationalDatabaseTests
+{
+    public sealed class UsingTests
+    {
+        private RelationalDatabase _database;
+
+        [OneTimeSetUp]
+        public void Setup() 
+            => _database = new RelationalDatabasesFactory().Create();
+
+        [Test]
+        public void IsEscapingCharactersCorrect1()
+        {
+            Assert.That(() =>
+            {
+                var method = _database.GetType().GetMethod("AddEscapingCharactersToString", BindingFlags.NonPublic | BindingFlags.Instance);
+                var resultRequest = (string)method?.Invoke(_database, new object[] { "select * from !@#$%^&*()_+\\/,\"'" });
+                return resultRequest == "select * from !@#$%^&*()_+\\/,\"''";
+            });
+        }
+
+        [Test]
+        public void IsEscapingCharactersCorrect2()
+        {
+            Assert.Throws<TargetInvocationException>(() =>
+            {
+                var method = _database.GetType().GetMethod("AddEscapingCharactersToString", BindingFlags.NonPublic | BindingFlags.Instance);
+                method?.Invoke(_database, new object[] { null });
+            });
+        }
+    }
+}
